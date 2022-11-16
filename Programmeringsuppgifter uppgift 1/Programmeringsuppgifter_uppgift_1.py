51dfbdcb3a076289931b26503457044ee698b556
Admin <Admin@TEB123012>
2022-11-16 10:06:57 +0100

    Add project files.

---
+++ Programmeringsuppgifter uppgift 1/Programmeringsuppgifter_uppgift_1.py
@@ -0,0 +1,19 @@
+ruta=" "*50
+print(ruta,"+","-"*16,"+")
+print(ruta,"|  Te21C           |")
+print(ruta,"|  Anton Ahlm      |")
+print(ruta,"+","-"*16,"+ \n\n")
+
+# A
+print(4.2/(7.3+0.26))
+
+#B
+print(2**6)
+
+#C
+print(200**0.5)
+
+#D
+print("Pa 1100 minuter gar det ",int(1100/60),"timmar och ",1100%60,"mintuer.")
+
+
